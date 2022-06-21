6b15a947671a33580597c75d1ddf8d7210b6ab52
David Samuelson <linksn.1fan@gmail.com>
2022-06-21 10:07:45 -0600

    finished indexing products and functions

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -14,6 +14,7 @@ import {
 } from '../utils/actions';
 
 import Cart from '../components/Cart';
+import { idbPromise } from '../utils/helpers';
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
@@ -34,11 +35,16 @@ function Detail() {
         _id: id,
         purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
       });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
     } else {
       dispatch({
         type: ADD_TO_CART,
         product: { ...currentProduct, purchaseQuantity: 1 }
       });
+      idbPromise('cart', 'put', {...currentProduct, purchaseQuantity: 1} );
     }
   };
 
@@ -46,7 +52,8 @@ function Detail() {
     dispatch({
       type: REMOVE_FROM_CART,
       _id: currentProduct._id
-    })
+    });
+    idbPromise('cart', 'delete', { ...currentProduct } );
   }
 
   useEffect(() => {
@@ -56,9 +63,19 @@ function Detail() {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
+      });
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      })
+    } else if (!loading) {
+      idbPromise('products', 'get').then((indexedProducts) => {
+        dispatch({ 
+          type: UPDATE_PRODUCTS,
+          products: indexedProducts
+        })
       })
     }
-  }, [products, data, dispatch, id]);
+  }, [products, data, loading, dispatch, id]);
 
   return (
     <>
